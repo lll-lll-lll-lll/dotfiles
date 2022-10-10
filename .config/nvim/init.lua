8513aa4c41fb7a21426052dc32d5a0475d1a613a
0xpeIpeI <hokumaru12@gmail.com>
2022-10-10 12:59:04 +0900

    modify: plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,10 @@
 require('user.keymaps')
 require('user.opt')
+require('plugins')
+
+require('packer').startup(function()
+    use 'wbthomason/packer.nvim'
+    use 'tomasr/molokai'
+end)
+
+vim.g.colors_name = 'molokai'
